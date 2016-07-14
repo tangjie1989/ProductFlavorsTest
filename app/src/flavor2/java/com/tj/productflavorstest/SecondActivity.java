01fec2471c8064e4c3b806e74986cb030af11287
tangjie <tangjie@renrenche.com>
2016-07-14 14:04:11 +0800

    code clean
    code clean

--- app/src/flavor2/java/com/tj/productflavorstest/SecondActivity.java
+++ app/src/flavor2/java/com/tj/productflavorstest/SecondActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
